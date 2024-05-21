0478efcc221df919714214fbe639480440652200
Samuel Cortés Sánchez <samuelcortessanchez@gmail.com>
2024-05-21 15:53:47 +0200

    UserValidatorTest

--- CineFx/demo/src/main/kotlin/org/example/demo/usuarios/validator/UserValidator.kt
+++ CineFx/demo/src/main/kotlin/org/example/demo/usuarios/validator/UserValidator.kt
@@ -7,45 +7,42 @@ import org.example.demo.usuarios.errors.UserError
 import org.example.demo.usuarios.models.Usuario
 
 fun Usuario.validate(): Result<Usuario, UserError>{
-        if (this.nombre.isEmpty() || this.nombre.isBlank()){
-            return Err(UserError.ValidateProblem("El nombre no puede estar vacío"))
-        }
-        if (this.nombre.isEmpty() || this.nombre.isBlank()){
-            return Err(UserError.ValidateProblem("El nombre no puede estar vacío"))
-        }
-        if (this.apellidos.isEmpty() || this.apellidos.isBlank()){
-            return Err(UserError.ValidateProblem("Los apellidos no pueden estar vacios"))
-        }
-        val regexMail = Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,3}$")
-        if(!this.email.matches(regexMail)){
-            return Err(UserError.ValidateProblem("El correo electrónico no es correcto"))
-        }
-        val regexContraseña = Regex("^(?=.[a-z])(?=.[A-Z])(?=.\\d)(?=.[@\$!%?&])[A-Za-z\\d@\$!%?&]{12,}$")
-        if(!this.contraseña.matches(regexContraseña) || this.contraseña.isEmpty() || this.contraseña.isBlank()){
-            return Err(UserError.ValidateProblem("La contraseña no es válida, debe tener 12 carácteres, contener al menos una mayúscula y una minúsucla, un número y un caracter especial"))
-        }
-        return Ok(this)
+    if (this.nombre.isEmpty() || this.nombre.isBlank()){
+        return Err(UserError.ValidateProblem("El nombre no puede estar vacío"))
+    }
+    if (this.nombre.isEmpty() || this.nombre.isBlank()){
+        return Err(UserError.ValidateProblem("El nombre no puede estar vacío"))
+    }
+    if (this.apellidos.isEmpty() || this.apellidos.isBlank()){
+        return Err(UserError.ValidateProblem("Los apellidos no pueden estar vacios"))
+    }
+    val regexMail = Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,3}$")
+    if(!this.email.matches(regexMail)){
+        return Err(UserError.ValidateProblem("El correo electrónico no es correcto"))
+    }
+    val regexContraseña = Regex("^(?=.[a-z])(?=.[A-Z])(?=.\\d)(?=.[@\$!%?&])[A-Za-z\\d@\$!%?&]{12,}$")
+    if(!this.contraseña.matches(regexContraseña) || this.contraseña.isEmpty() || this.contraseña.isBlank()){
+        return Err(UserError.ValidateProblem("La contraseña no es válida, debe tener 12 carácteres, contener al menos una mayúscula y una minúsucla, un número y un caracter especial"))
+    }
+    return Ok(this)
 }
 
 
-
- fun validateNombre(nombre: String): Boolean{
+fun validateNombre(nombre: String): Boolean{
     if (nombre.isEmpty() || nombre.isBlank()){
         return false
     }
     return true
 }
 
- fun validateApellidos(apellidos: String): Boolean{
+fun validateApellidos(apellidos: String): Boolean{
     if (apellidos.isEmpty() || apellidos.isBlank()){
         return false
-
     }
     return true
 }
 
-
-fun validateEmail(usuario: Usuario): Result<String, UserError>{
+fun validateEmail(email: String): Boolean{
     val regex = Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
     if(!email.matches(regex)){
         return false
@@ -53,8 +50,8 @@ fun validateEmail(usuario: Usuario): Result<String, UserError>{
     return true
 }
 
-fun validateContraseña(usuario: Usuario): Result<String, UserError>{
-    val regex = Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@\$!%*?&])[A-Za-z\\d@\$!%*?&]{12,}$")
+fun validateContraseña(contraseña: String): Boolean{
+    val regex = Regex("^(?=.[a-z])(?=.[A-Z])(?=.\\d)(?=.[@\$!%?&])[A-Za-z\\d@\$!%?&]{12,}$")
     if(!contraseña.matches(regex)){
         return false
     }
